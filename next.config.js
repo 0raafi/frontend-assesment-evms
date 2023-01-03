61f3065ed92882c1a891f1f1f67c199c61079fa7
Raafi <rafmhmd06@gmail.com>
2023-01-03 23:38:46 +0700

    + Fix next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    apiUrl: process.env.API_URL
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
